3da523633943c25e128421abb8f452e60c6e3889
Sori <ljgggg1234@gmail.com>
2023-09-12 13:43:30 +0000

    [TDD] email register

---
+++ testcode/emailRegister.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const register = async()=>{
+	console.log("[!] TEST register start!")
+	/*
+	{
+	"email":"sori@email.com",
+	"password":"z",
+	"name":"jingeun",
+	"phone":"+821012341234",
+	"nickname":"xiaxia",
+	"address":"kokookokoko",
+	"address_detail":"zz",
+    	"platform":4,
+	"noti_flag":1
+	}
+	*/
+	let data = await axios(
+		{
+			url :"https://ss-dev.noe.systems/users/join",
+			method : "POST",
+			data : 	{
+				"email":"sori@eemail.com",
+				"password":"z",
+				"name":"jingeun",
+				"phone":"+821012341234",
+				"nickname":"xiaxia",
+				"address":"kokookokoko",
+				"address_detail":"zz",
+			    	"platform":4,
+				"noti_flag":1
+			}
+	})
+	console.log(data.data);
+	console.log("[=] test end..");
+}
+register();
