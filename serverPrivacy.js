cde62d013a9b006b5135d6bc64f4a2e9e839966c
Sori <ljgggg1234@gmail.com>
2023-09-12 11:51:21 +0000

    db config add

---
+++ serverPrivacy.js
@@ -0,0 +1,12 @@
+const dbID = "dbID";
+const dbPW = "dbPW";
+const dbHOST = "dbHOST";
+const dbNAME = "dbNAME";
+
+module.exports = {
+	dbID : dbID,
+	dbPW : dbPW,
+	dbHOST : dbHOST,
+	dbNAME : dbNAME
+}
+
