2dfe4dfebefc1b0fa5b224cb811eebaf5988756a
Sori <ljgggg1234@gmail.com>
2023-09-12 12:21:44 +0000

    bodyParser apply

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import Router from 'koa-router';
 const PORT = 8080;
 const router = new Router();
 import index from './routes/index.js';
-
+import bodyParser from 'koa-bodyparser';
+app.use(bodyParser());
 router.use(index.routes());
 app.use(router.routes());
 app.use(router.allowedMethods());
