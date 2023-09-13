63482f16667806820b3c884873af8ca6e0d2e003
Sori <ljgggg1234@gmail.com>
2023-09-13 08:37:53 +0000

    server info reform

--- serverPrivacy.js
+++ serverPrivacy.js
@@ -1,15 +1,29 @@
-const dbID = "root";
+const dbID = "";
 const dbPW = "";
 const dbHOST = "";
 const database = "";
-
+const naverClientID = "";
+const naverClientSecret = "";
+const kakaoClientID = "";
+const kakaoClientSecret = "";
+const googleClientID = "";
+const googleClientSecret = "";
+const domain = "";
 module.exports = {
-	client : "mysql",
-	connection:{
-		user : dbID,
-		password : dbPW,
-		host : dbHOST,
-		database : database
-	}
+    client : "",
+    connection:{
+        user : dbID,
+        password : dbPW,
+        host : dbHOST,
+        database : database
+    },
+	jwtKey : "",
+    naverClientID : naverClientID,
+    naverClientSecret : naverClientSecret,
+    kakaoClientID : kakaoClientID,
+    kakaoClientSecret : kakaoClientSecret,
+    googleClientID : googleClientID,
+    googleClientSecret : googleClientSecret,
+    domain : domain
 }
 
