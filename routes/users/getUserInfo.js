60192b0956a62f641afd5329c78cbf27305bc7e1
Sori <ljgggg1234@gmail.com>
2023-09-19 18:19:29 +0900

    Ajou 26 (#6)
    * AJOU-26 /:idx complate
    
    * AJOU-26 testcode upload..

--- routes/users/getUserInfo.js
+++ routes/users/getUserInfo.js
@@ -6,8 +6,33 @@ import knex from "knex";
 import jwt from 'jsonwebtoken';
 import {tokenCheck} from './tokenCheck.js';
 const conn = knex({client:client, connection:connection});
+router.get('/:idx',async(ctx)=>{
+	console.log("idx");
+	const {idx} = ctx.params;
+	const {authorization} = ctx.request.header;
+	if(tokenCheck(authorization)){
+		ctx.body = {"status":"ok","code":-1, "text":"invalid token"}
+		return;
+	}
+	else{
+		var decoded = jwt.verify(authorization, jwtKey);
+		console.log(decoded);
+		const users = await conn("users").select().where({idx:idx});
+		if(users.length != 1){
+			ctx.body = {"status":"no", "code":-1, "text":"invalid user"};
+		}
+		else{
+			delete users[0].password;
+			delete users[0].is_delete;
+			delete users[0].delete_date;
+			ctx.body={"status":"ok","code":1,"text":"complate","userInfo":users[0]};
+		}
+	}
+});
+
 router.get('/',async(ctx)=>{
 	const {authorization} = ctx.request.header;
+	console.log(ctx.request)
 	if(tokenCheck(authorization)){
 		ctx.body = {"status":"ok","code":-1, "text":"invalid token"}
 		return;
@@ -27,7 +52,6 @@ router.get('/',async(ctx)=>{
 		}
 	}
 });
-
 module.exports = router;
 
 
