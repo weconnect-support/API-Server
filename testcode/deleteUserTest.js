d56c01ad92b6d2821d8f5b1b066c10001e9ddbf6
Sori <ljgggg1234@gmail.com>
2023-10-05 01:00:11 +0900

    Ajou 24 (#11) / 회원 탈퇴 API 구현 (soft delete)
    * user delete complate
    
    * account delete test complate

--- testcode/deleteUserTest.js
+++ testcode/deleteUserTest.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZHgiOjUsIm5pY2tuYW1lIjoibW9tbyIsImV4cGlyZSI6IjIwMjMtMDktMzBUMTQ6MTU6MjkuMTY1WiIsImlhdCI6MTY5NjA4MzMyOX0.jhq10HQ3C6_5bVTrKZb6-uyktKyItLnYrvKAJldqETs" 
-const getUser = async()=>{
-	console.log("[!] Get User Infomation TEST!")
+const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZHgiOjUsIm5pY2tuYW1lIjoibW9tbyIsImV4cGlyZSI6IjIwMjMtMDktMzBUMTQ6MTU6MjkuMTY1WiIsImlhdCI6MTY5NjA4MzMyOX0.jhq10HQ3C6_5bVTrKZb6-uyktKyItLnYrvKAJldqETs"
+const delUser = async()=>{
+	console.log("[!] Delete User Account TEST!")
+	await getUserIdx(5)
 	let data = await axios(
 		{
 			url :"https://api-dev.weconnect.support/users",
-			method : "GET",
+			method : "DELETE",
 			headers : {'authorization':jwt}
 
 	})
@@ -17,7 +18,7 @@ const getUserIdx = async(idx)=>{
 	console.log("[!] Get User Infomation using idx TEST!")
 	let data = await axios(
 		{
-			url :`https://api-dev.weconnect.support/users/${idx}`,
+			url :`https://ss-dev.noe.systems/users/${idx}`,
 			method : "GET",
 			headers : {'authorization':jwt}
 
@@ -25,5 +26,4 @@ const getUserIdx = async(idx)=>{
 	console.log(data.data);
 	console.log("[=] test end..");
 }
-getUser();
-getUserIdx(5);
+delUser();
