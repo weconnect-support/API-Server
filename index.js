2fd695fd6b97b510e204a950d827c4ce5b103585
Sori <ljgggg1234@gmail.com>
2023-10-04 17:35:00 +0000

    koa cors dev version apply > @localhost

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import Koa from 'koa';
 const app = new Koa();
 import Router from 'koa-router';
 const PORT = 8080;
+const cors = require('@koa/cors');
+
+let corsOptions = {
+	origin: "http://localhost:3000",
+	credentials: true,
+}
+app.use(cors(corsOptions)); 
 const router = new Router();
 import index from './routes/index.js';
 import bodyParser from 'koa-bodyparser';
