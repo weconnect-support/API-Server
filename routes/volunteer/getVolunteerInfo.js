753fd6f759aace88425401ae68217012ce81eb5c
Sori <ljgggg1234@gmail.com>
2023-10-05 08:44:19 +0000

    volunteer & comment connect

--- routes/volunteer/getVolunteerInfo.js
+++ routes/volunteer/getVolunteerInfo.js
@@ -9,23 +9,31 @@ const conn = knex({client:client, connection:connection});
 router.get('/:idx',async(ctx)=>{
 	console.log("inex");
 	const {idx} = ctx.params;
-	const {authorization} = ctx.request.header;
-	/*if(tokenCheck(authorization)){
-		ctx.body = {"status":"ok","code":-1, "text":"invalid token"}
-		return;
+	const volunteers = await conn("volunteers")
+		.join('users','volunteers.user_idx','=','users.idx')
+		.select('volunteers.title','volunteers.detail','volunteers.location','users.nickname','users.name','users.email')
+		.where({"volunteers.is_delete":0});
+	const comment = await conn("comment")
+		.join('users','comment.user_idx', '=', 'users.idx')
+		.select("comment.*", "users.name","users.nickname")
+		.where({
+			"comment.is_delete":0, 
+			"comment.volunteer_idx":idx
+		})
+	if(volunteers.length == 0){
+		ctx.body = {"status":"ok","code":0,"text":"invalid idx"}
 	}
 	else{
-	
-		var decoded = jwt.verify(authorization, jwtKey);
-		console.log(decoded);
-		*/
-		const volunteers = await conn("volunteers").select().where({idx:idx}).andWhereNot({"is_delete":"1"});
-		if(volunteers.length == 0){
-			ctx.body = {"status":"ok","code":0,"text":"invalid idx"}
+		ctx.body = {
+			"status":"ok",
+			"code":1,
+			"data":{
+				"volunteer":volunteers[0],
+				"comments":comment,
+			},
+			"text":"volunteers data complate"
 		}
-		else{
-			ctx.body = {"status":"ok","code":1,"data":volunteers[0],"text":"volunteers data complate"}
-		}
+	}
 	///}
 });
 router.get('/',async(ctx)=>{
@@ -39,7 +47,10 @@ router.get('/',async(ctx)=>{
 		var decoded = jwt.verify(authorization, jwtKey);
 		console.log(decoded);
 		*/
-		const volunteers = await conn("volunteers").select().where({is_delete:0});
+		const volunteers = await conn("volunteers")
+		.join('users','volunteers.user_idx','=','users.idx')
+		.select('volunteers.title','volunteers.detail','volunteers.location','users.nickname','users.name','users.email')
+		.where({"volunteers.is_delete":0});
 		if(volunteers.length == 0){
 			ctx.body = {"status":"ok","code":0,"text":"invalid data"}
 		}
