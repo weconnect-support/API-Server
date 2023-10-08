16648494961c968ec2402d9d5136f62eb7e03153
Sori <ljgggg1234@gmail.com>
2023-10-08 07:46:18 +0000

    user account infomation modify api complate.. jira issue ajou-25

--- routes/users/login.js
+++ routes/users/login.js
@@ -7,34 +7,6 @@ import crypto from "crypto";
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
 const conn = knex({client:client, connection:connection})
-var state = "RANDOM_STATE";
-var redirectURI = encodeURI(domain+"/users/login/naver");
-var api_url = "";
-//google authorization url
-const oauth2Client = new google.auth.OAuth2(
-	googleClientID,
-	googleClientSecret,
-	domain+"/users/login/google"
-);
-const scopes = [
-	"https://www.googleapis.com/auth/userinfo.profile",//
-	"https://www.googleapis.com/auth/user.phonenumbers.read",
-	"email",
-	"profile",
-	'phone'
-];
-const authorizationUrl = oauth2Client.generateAuthUrl({
-	access_type: 'offline',
-	scope: scopes,
-	include_granted_scopes: true
-});
-
-router.get('/', (ctx)=> {
-	api_url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naverClientID}&redirect_uri=${redirectURI}&state=${state}`;
-	let kakaourl = "https://kauth.kakao.com/oauth/authorize?client_id=eb14cc7280926662ac5976efbbe3943a&redirect_uri=https://ss-dev.noe.systems/users/login/kakao&response_type=code&scope=profile_nickname,account_email,gender,birthday"
-	ctx.body = "<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a><br><h3>"+authorizationUrl+"</h3><br>"+kakaourl;
-	
-});
 const accountCheck = async(email, platform)=>{
 	let signupCheck = await conn("users").select().where({email : email, platform : platform, is_delete:0})
 	if(signupCheck.length == 0)
@@ -46,24 +18,27 @@ const accountCheck = async(email, platform)=>{
 router.post('/',async(ctx)=>{	
 	const {platform} =  ctx.request.body;
 	if(platform == undefined || (platform >= 5 || platform <= 0)){
-		/*
-		console.log(platform);
-		console.log(platform == undefined);
-		console.log(platform >= 5);
-		console.log(platform <= 0);
-		*/
 		ctx.body = {"status":"no", "code":-1, "text":"platform not found"};
+		return;
 	}
 	else if(platform == 4){
 		const {email, password} = ctx.request.body;
+		if(!email || !password){
+			ctx.body = {"status":"no","code": -1, "text":"parameter validation check error"}
+			return;
+		}
+			console.log("aa");
 		let userCheck = await conn("users").select().where({email:email, password:crypto.createHash('sha512').update(password).digest('hex'), is_delete: 0});
+		console.log("dd");
 		if(userCheck.length == 1){
 			let {idx, email,nickname}  = userCheck[0];
 			let token = jwt.sign({"idx":idx,"nickname":nickname,"expire":new Date()}, jwtKey);
 			ctx.body = {"status":"ok","code":1,token:token,"text":"login_success"}
+			return;
 		}
 		else{
 			ctx.body = {"status":"no","code":2,"text":"login_fail"}
+			return;
 		}
 	}
 	else{
--- routes/users/updateUserInfo.js
+++ routes/users/updateUserInfo.js
@@ -1,6 +1,5 @@
 import Router from 'koa-router';
 const router = new Router();
-import {google} from 'googleapis'
 import {jwtKey, client, connection} from '../../serverPrivacy.js';
 import knex from 'knex';
 import crypto from "crypto";
@@ -10,7 +9,45 @@ const conn = knex({client:client, connection:connection})
 import {tokenCheck} from "../util/tokenCheck.js";
 router.put('/', async(ctx)=> {
 	const {authorization} = ctx.request.header;
-	console.log(ctx.request)
+	if(!ctx.request.body.password){
+		ctx.body = {"status":"no","code":-2, "text":"invalid parameter"};
+		return;
+	}
+	if(tokenCheck(authorization)){
+		ctx.body = {"status":"no","code":-1, "text":"invalid token"}
+		return;
+	}
+	else{
+		var decode = jwt.verify(authorization, jwtKey);
+		var res;
+		var {password} = ctx.request.body
+		var token;
+
+		try{
+			res = await conn("users").update({
+				"password":crypto.createHash('sha512')
+				.update(password)
+				.digest('hex'),
+				"last_modify_time": conn.raw("now()")
+			}).where({idx:decode.idx});
+			res = await conn("users").select().where({idx:decode.idx, platform:4});
+			delete res[0].password
+			token = jwt.sign({"idx":res[0].idx,"nickname":res[0].nickname,"expire":new Date(), platform : res[0].platform},jwtKey);
+
+		}
+		catch(e){
+			ctx.body = {"status":"no","code":-1, "text":"fail.."}
+		}
+		ctx.body = {"status":"ok","code":1,"text":"modify_complate",token:token}
+	}
+})
+router.patch('/', async(ctx)=> {
+	const {authorization} = ctx.request.header;
+	const {nickname, name, phone, address, address_detail} = ctx.request.body;
+	if(!nickname || !name || !phone || !address || !address_detail){
+		ctx.body = {"status":"no","code":-2, "text":"invalid parameter"};
+		return;
+	}
 	if(tokenCheck(authorization)){
 		ctx.body = {"status":"ok","code":-1, "text":"invalid token"}
 		return;
@@ -18,26 +55,25 @@ router.put('/', async(ctx)=> {
 	else{
 		var decode = jwt.verify(authorization, jwtKey);
 		var res;
-		/*
-		var option = ctx.request.body;
-		delete option.platform;
-		delete option.name;
-		*/
-		var nick = ctx.request.body.nickname
 		var token;
 		try{
-			res = await conn("users").update({"nickname":nick,"last_modify_time":conn.raw("now()")}).where({idx:decode.idx});
+			res = await conn("users").update({
+				"nickname":nickname,
+				"name" : name,
+				"phone":phone,
+				"address":address,
+				"address_detail" : address_detail,
+				"last_modify_time":conn.raw("now()")
+			}).where({idx:decode.idx});
 			res = await conn("users").select().where({idx:decode.idx});
-			delete res[0].password
-			token = jwt.sign({"idx":res[0].idx,"nickname":res[0].nickname,"expire":new Date()},jwtKey);
+			token = jwt.sign({"idx":res[0].idx,"nickname":res[0].nickname,"expire":new Date(),"platform":res[0].platform},jwtKey);
 
 		}
 		catch(e){
 			console.log(e);
 			ctx.body = {"status":"no","code":-1, "text":"fail.."}
 		}
-		ctx.body = {"status":"ok","code":1,"text":"data_complate","data" : decode, res:res[0],token:token}
+		ctx.body = {"status":"ok","code":1,"text":"data_modify_complate",token:token}
 	}
 })
-
 module.exports = router;
