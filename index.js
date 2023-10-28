03a55453ce4c7d82e9ac0b61f25b6c54c878ee5d
Sori <ljgggg1234@gmail.com>
2023-10-28 04:45:48 +0000

    typo err fix.. at index.js

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen(PORT, () => {
-    console.log(`ssangsnag API Server START : PORT - ${PORT}`);
+    console.log(`ssangsang API Server START : PORT - ${PORT}`);
 });
